2cfdab62375ae02ee26744b1e88ba6102f67d45d
Ruud Gerrits <ruud.gerrits@live.nl>
2014-12-05 12:47:33 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,21 @@
-var test_form = new Validator(".test_form", {
-    name: "required|alphanum|min:3|max:64",
-    ammount: "required|integer|min-value:30|max-value:40",
-    text: "required|nofollow|alphanum|min:10|max:100"
-});
-test_form.setDebug(true);
-test_form.setSpecificErrorMessageLocation("text", ".error-messages");
-test_form.setCallback(function(type, name, field, validators, errors){
-    if(type === "SUBMIT") {
-        if (name === "text") {
-            if (Object.keys(errors).length >= 1) {
-                jQuery(".error-messages").fadeIn();
-            }
-            else {
-                jQuery(".error-messages").fadeOut();
-            }
-        }
-    }
+jQuery(document).ready(function(){
+	var test_form = new Validator(".test_form", {
+		"name": "required|alphanum|min:3|max:64",
+		"ammount": "required|integer|min-value:30|max-value:40",
+		"textarea.text": "required|nofollow|alphanum|min:10|max:100"
+	});
+	test_form.setDebug(true);
+	test_form.setSpecificErrorMessageLocation("text", ".error-messages");
+	test_form.setCallback(function(type, name, field, validators, errors){
+		if(type === "SUBMIT") {
+			if (name === "text") {
+				if (Object.keys(errors).length >= 1) {
+					jQuery(".error-messages").fadeIn();
+				}
+				else {
+					jQuery(".error-messages").fadeOut();
+				}
+			}
+		}
+	});
 });
